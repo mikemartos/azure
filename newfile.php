6079c1eb38d673a47f7812e7084a1f6ebaf3bcb6
mikemartos <mike.martos@live.com>
2014-08-21 20:01:59 -0600

    db

--- newfile.php
+++ newfile.php
@@ -3,5 +3,20 @@ echo 'this is my first page \n';
 
 echo 'how is it going';
 
+
+/*
+ PHP MSSQL Example
+
+Replace data_source_name with the name of your data source.
+Replace database_username and database_password
+with the SQL Server database username and password.
+*/
+$data_source='ry76ak4ig9.database.windows.net';
+$user='mike';
+$password='Ms10kl31011'
+		
+		
+		
+
 ?>
 
