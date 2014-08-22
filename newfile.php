53c7f6ab03f55358c71e78363e568c4d76aaead6
mikemartos <mike.martos@live.com>
2014-08-21 20:15:17 -0600

    pull data

--- newfile.php
+++ newfile.php
@@ -13,10 +13,15 @@ with the SQL Server database username and password.
 */
 $data_source='ry76ak4ig9.database.windows.net';
 $user='mike';
-$password='Ms10kl31011'
+$password='Ms10kl31011';
 		
+// This query generates a result set with one record in it.
+$sql="SELECT * FROM TEST";
+
+# Execute the statement.
+$rs=odbc_exec($conn,$sql);		
 		
-		
+ECHO $rs
 
 ?>
 
