f9c88ca61fa69b1a109f4869c3ed68241966b5e8
mikemartos <mike.martos@live.com>
2014-08-21 20:44:08 -0600

    q

--- newfile.php
+++ newfile.php
@@ -1,13 +1,6 @@
 <?php
 echo 'this is my first page ';
 
-/*
- PHP MSSQL Example
-
-Replace data_source_name with the name of your data source.
-Replace database_username and database_password
-with the SQL Server database username and password.
-*/
 $myServer = "ry76ak4ig9.database.windows.net";
 $myUser = "mike";
 $myPass = "Ms10kl31011";
@@ -32,13 +25,9 @@ $numRows = mssql_num_rows($result);
 echo "<h1>" . $numRows . " Row" . ($numRows == 1 ? "" : "s") . " Returned </h1>";
 
 //display the results
-while($row = mssql_fetch_array($result))
-{
-	echo "<li>" . $row["id1"] . "</li>";
-}
+
 //close the connection
 mssql_close($dbhandle);
-
 
 ?>
 
