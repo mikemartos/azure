9b563ab651280332dd15d7bbf796da2c94e73606
mikemartos <mike.martos@live.com>
2014-08-21 21:11:49 -0600

    g

--- newfile.php
+++ newfile.php
@@ -1,7 +1,7 @@
 <?php
 echo 'this is my first page ';
 
-$myServer = "ry76ak4ig9.database.windows.net,1433";
+$myServer = "ry76ak4ig9.database.windows.net";
 $myUser = "mike";
 $myPass = "Ms10kl31011";
 $myDB = "gedtest";
@@ -9,10 +9,6 @@ $myDB = "gedtest";
 //connection to the database
 $dbhandle = mssql_connect($myServer, $myUser, $myPass)
 or die("Couldn't connect to SQL Server on $myServer");
-
-//select a database to work with
-$selected = mssql_select_db($myDB, $dbhandle)
-or die("Couldn't open database $myDB");
 
 
 
