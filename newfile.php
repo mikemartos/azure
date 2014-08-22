7e4a5d43c2d1830e2c4ccefc514d87149c7e2550
mikemartos <mike.martos@live.com>
2014-08-21 20:23:49 -0600

    count1

--- newfile.php
+++ newfile.php
@@ -14,9 +14,18 @@ with the SQL Server database username and password.
 $data_source='ry76ak4ig9.database.windows.net';
 $user='mike';
 $password='Ms10kl31011';
-		
+
+$conn=odbc_connect($data_source,$user,$password);
+if (!$conn){
+	if (phpversion() < '4.0'){
+		exit("Connection Failed: . $php_errormsg" );
+	}
+	else{
+		exit("Connection Failed:" . odbc_errormsg() );
+	}
+}
 // This query generates a result set with one record in it.
-$sql="SELECT count(*) FROM TEST";
+$sql="SELECT count(*) FROM dbo.test";
 
 # Execute the statement.
 $rs=odbc_exec($conn,$sql);		
