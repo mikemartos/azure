52e47a7342e292b0b7eafbb7b575ac9ef87dfc13
mikemartos <mike.martos@live.com>
2014-08-21 20:55:20 -0600

    a

--- newfile.php
+++ newfile.php
@@ -10,6 +10,10 @@ $myDB = "gedtest";
 $dbhandle = mssql_connect($myServer, $myUser, $myPass)
 or die("Couldn't connect to SQL Server on $myServer");
 
+//select a database to work with
+$selected = mssql_select_db($myDB, $dbhandle)
+or die("Couldn't open database $myDB");
+
 
 
 ?>
