91752723afd560c96c6775c96b6ce9f6678040ce
mikemartos <mike.martos@live.com>
2014-08-21 20:46:06 -0600

    a

--- newfile.php
+++ newfile.php
@@ -10,24 +10,7 @@ $myDB = "gedtest";
 $dbhandle = mssql_connect($myServer, $myUser, $myPass)
   or die("Couldn't connect to SQL Server on $myServer"); 
 
-//select a database to work with
-$selected = mssql_select_db($myDB, $dbhandle)
-or die("Couldn't open database $myDB");
 
-//declare the SQL statement that will query the database
-$query = "SELECT * ";
-$query .= "FROM test ";
-
-//execute the SQL query and return records
-$result = mssql_query($query);
-
-$numRows = mssql_num_rows($result);
-echo "<h1>" . $numRows . " Row" . ($numRows == 1 ? "" : "s") . " Returned </h1>";
-
-//display the results
-
-//close the connection
-mssql_close($dbhandle);
 
 ?>
 
